963d6bc056b9b8a90bf588b66a0b0d56eaee80fe
dj1n19 <bruno.genie@hotmail.com>
2022-04-08 13:50:54 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,76 +6,100 @@
 /*   By: bgenie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 09:48:58 by bgenie            #+#    #+#             */
-/*   Updated: 2022/04/07 18:19:28 by bgenie           ###   ########.fr       */
+/*   Updated: 2022/04/08 13:49:57 by bgenie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
-//#include <stdio.h>
+#include <stdio.h>
 
-static int	ft_count_delim(char const *s, char c)
+static int	ft_count_delim(char *s, char c)
 {
-	int	i;
 	int	count;
 
-	i = 0;
 	count = 0;
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] == c)
-		{	
+		if (*s == c)
 			count++;
-			while (s[i] == c)
-				i++;
-			if (!s[i])
-			{
-				count--;
-				return (count);
-			}
-		}
-		i++;
+		while(*s == c)
+			s++;
+		s++;
 	}
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static char	*ft_trim(char const *s, char c)
 {
-	char		**strings_array;
-	char		**strings_array_start;
-	const char	*s_start;
-	int			i;
+	char	*trim;
+	int		i;
 
 	while (*s == c)
 		s++;
-	strings_array = malloc(sizeof(char *) * (ft_count_delim(s, c) + 2));
-	//printf("%d\n", ft_count_delim(s, c));
-	if (!strings_array)
-		return (NULL);
-	strings_array_start = strings_array;
-	s_start = s;
-	while(*s)
-	{
-		i = 0;
-		while (*s != c)
-		{
-			s++;
-			i++;
-		}
-		*strings_array++ = ft_substr(s_start, 0, i);
-		while (*s == c)
-			s++;
-		s_start = s;
-	}
-	*strings_array = NULL;
-	return (strings_array_start);
+	i = ft_strlen(s) - 1;
+	while (s[i] == c)
+		i--;
+	trim = ft_substr(s, 0, i + 1);
+	return (trim);
 }
 
-/*int main()
+static char	*ft_word_extract(char *s, char c)
+{
+	char	*word;
+	char	*start;
+	int	i;
+
+	start = s;
+	i = 0;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			word = ft_substr(start, 0, i);
+			return (word);
+		}
+		i++;
+		s++;
+	}
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strings_array;
+	char	**sa_start;
+	char	*tmp;
+
+	tmp = ft_trim(s, c);
+	strings_array = malloc(sizeof(char *) * (ft_count_delim(tmp, c) + 1));
+	if (!strings_array)
+		return (NULL);
+	sa_start = strings_array;
+	while(*tmp)
+	{
+		if (*tmp != c)
+		{
+			*strings_array = ft_word_extract(tmp, c);
+			if (!strings_array)
+				return (NULL);
+			tmp += ft_strlen(*strings_array);
+			printf("%s\n", tmp);
+			strings_array++;
+		}
+		else
+			tmp++;
+	}
+	*strings_array = 0;
+	free(tmp);
+	return (sa_start);
+}
+
+int main()
 {
 	char **tab = ft_split("xxtripouillexx42xx", 'x');
 	printf("%s\n", tab[0]);
 	printf("%s\n", tab[1]);
 	printf("%s\n", tab[2]);
 	return (0);
-}*/
+}
